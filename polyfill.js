d95b327a3d5b9153f6686c9ee4f06d59555040f1
Ashley Claymore <acutmore@users.noreply.github.com>
2021-04-11 16:02:25 +0100

    typed array polyfill

--- polyfill.js
+++ polyfill.js
@@ -81,3 +81,211 @@
 
     Object.defineProperties(arrayPrototype, propertyDescriptors);
 })(Array.prototype);
+
+((typedArrayPrototype) => {
+    /** @typedef {Int8Array} TypedArray */
+
+    function toIntegerOrInfinity(arg) {
+        let n = Number(arg);
+        if (Number.isNaN(n) || n === 0) {
+            return 0;
+        }
+        if (n === Number.POSITIVE_INFINITY) {
+            return Number.POSITIVE_INFINITY
+        };
+        if (n === Number.NEGATIVE_INFINITY) {
+            return Number.NEGATIVE_INFINITY;
+        }
+        let i = Math.floor(Math.abs(n));
+        if (n < 0) {
+            i = -i;
+        }
+        return i;
+    }
+
+    /**
+     * @param {unknown} v
+     * @returns {asserts v is TypedArray}
+     */
+    function assertTypedArray(v) {
+        typedArrayPrototype.keys.call(v);
+    }
+
+    /**
+     * @param {TypedArray} species
+     * @param {number} length
+     * @returns {TypedArray}
+     */
+    function typedArraySpeciesCreate(species, length) {
+        assertTypedArray(species);
+        /** @type {any} */
+        const con = species.constructor;
+        return new con(length);
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["popped"]}
+     */
+    function popped() {
+        return typedArrayPrototype.slice.call(this, 0, -1);
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["pushed"]}
+     */
+    function pushed(...values) {
+        assertTypedArray(this);
+        const newLength = this.length + values.length;
+        const copy = typedArraySpeciesCreate(this, newLength);
+        let k = 0;
+        for (const v of this) {
+            copy[k++] = v;
+        }
+        for (const v of values) {
+            copy[k++] = v;
+        }
+        return copy;
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["reversed"]}
+     */
+    function reversed() {
+        assertTypedArray(this);
+        const copy = typedArrayPrototype.slice.call(this);
+        copy.reverse();
+        return copy;
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["shifted"]}
+     */
+    function shifted() {
+        return typedArrayPrototype.slice.call(this, 1);
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["sorted"]}
+     */
+    function sorted(compareFn) {
+        const copy = typedArrayPrototype.slice.call(this);
+        copy.sort(compareFn);
+        return copy;
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["spliced"]}
+     */
+     function spliced(start, deleteCount, ...values) {
+        assertTypedArray(this);
+        const len = this.length;
+        const relativeStart = toIntegerOrInfinity(start);
+        let actualStart;
+        if (relativeStart === -Infinity) {
+            actualStart = 0;
+        }
+        else if (relativeStart < 0) {
+            actualStart = Math.max(len + relativeStart, 0);
+        }
+        else {
+            actualStart = Math.min(relativeStart, len);
+        }
+        let insertCount;
+        let actualDeleteCount;
+        if (start === void 0) {
+            insertCount = 0;
+            actualDeleteCount = 0;
+        }
+        else if (deleteCount === void 0) {
+            insertCount = 0;
+            actualDeleteCount = len - actualStart;
+        }
+        else {
+            insertCount = values.length;
+            const dc = toIntegerOrInfinity(deleteCount);
+            actualDeleteCount = Math.max(0, Math.min(dc, len - actualStart))
+        }
+        const newLen = len + insertCount - actualDeleteCount;
+        const copy = typedArraySpeciesCreate(this, newLen);
+        let k = 0;
+        while (k < actualStart) {
+            copy[k] = this[k]
+            k++;
+        }
+        for (const E of values) {
+            copy[k] = E;
+            k++;
+        }
+        while (k < newLen) {
+            let from = k + actualDeleteCount - insertCount;
+            let fromValue = this[from];
+            copy[k] = fromValue;
+            k++;
+        }
+        return copy;
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["unshifted"]}
+     */
+    function unshifted(...values) {
+        assertTypedArray(this);
+        const newLength = values.length + this.length;
+        const copy = typedArraySpeciesCreate(this, newLength);
+        let k = 0;
+        for (const v of values) {
+            copy[k++] = v;
+        }
+        for (const v of this) {
+            copy[k++] = v;
+        }
+        return copy;
+    }
+
+    /**
+     * @this {TypedArray}
+     * @type {TypedArray["with"]}
+     */
+    function withFn(index, value) {
+        assertTypedArray(this);
+        const actualIndex = index < 0 ? this.length + index : index;
+        if (actualIndex < 0 || actualIndex >= this.length) {
+            throw new RangeError();
+        }
+        const copy = typedArrayPrototype.slice.call(this);
+        copy[index] = value;
+        return copy;
+    }
+
+    const additions = {
+        popped,
+        pushed,
+        reversed,
+        shifted,
+        sorted,
+        spliced,
+        unshifted,
+        with: withFn,
+    };
+
+    /** @type {PropertyDescriptorMap} */
+    const propertyDescriptors = {};
+
+    for (const [name, value] of Object.entries(additions)) {
+        propertyDescriptors[name] = {
+            enumerable: false,
+            configurable: true,
+            writable: true,
+            value,
+        };
+    }
+
+    Object.defineProperties(typedArrayPrototype, propertyDescriptors);
+})(Object.getPrototypeOf(Int8Array.prototype));
--- polyfill.test.js
+++ polyfill.test.js
@@ -119,3 +119,121 @@ tape("Array.prototype.with", (t) => {
     t.notDeepEqual(orig, copy);
     t.end();
 });
+
+tape("TypedArray.prototype.popped", (t) => {
+    const orig = new Int8Array([1, 2, 3]);
+    const expected = new Int8Array([1, 2]);
+
+    const copy = orig.popped();
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.pushed", (t) => {
+    const orig = new Int8Array([1, 2, 3]);
+    const push = [4, 5, 6];
+    const expected = new Int8Array([1, 2, 3, 4, 5, 6]);
+
+    const copy = orig.pushed(...push);
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.reversed", (t) => {
+    const orig = new Int8Array([3, 2, 1]);
+    const expected = new Int8Array([1, 2, 3]);
+
+    const copy = orig.reversed();
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.shifted", (t) => {
+    const orig = new Int8Array([1, 2, 3]);
+    const expected = new Int8Array([2, 3]);
+
+    const copy = orig.shifted();
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.sorted", (t) => {
+    const orig = new Int8Array([3, 1, 2]);
+    const expected = new Int8Array([1, 2, 3]);
+
+    const copy = orig.sorted();
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.sorted(compareFn)", (t) => {
+    const orig = new Int8Array([3, 1, 2]);
+    const expected = new Int8Array([3, 2, 1]);
+    function compareFn(a, b) {
+        return a > b ? -1 : 1;
+    }
+
+    const copy = orig.sorted(compareFn);
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.spliced", (t) => {
+    const orig = new Int8Array([1, -1, 0, -1, 4]);
+    const expected = new Int8Array([1, 2, 3, 4]);
+    const idx = 1;
+    const delNum = 3;
+    const ins = [2, 3];
+
+    const copy = orig.spliced(idx, delNum, ...ins);
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.unshifted", (t) => {
+    const orig = new Int8Array([4, 5, 6]);
+    const unshift = [1, 2, 3];
+    const expected = new Int8Array([1, 2, 3, 4, 5, 6]);
+
+    const copy = orig.unshifted(...unshift);
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
+
+tape("TypedArray.prototype.with", (t) => {
+    const orig = new Int8Array([1, 1, 3]);
+    const expected = new Int8Array([1, 2, 3]);
+    const idx = 1;
+    const val = 2;
+
+    const copy = orig.with(idx, val);
+
+    t.deepEqual(copy, expected);
+    t.notEqual(orig, copy);
+    t.notDeepEqual(orig, copy);
+    t.end();
+});
