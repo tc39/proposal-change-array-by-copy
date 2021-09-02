cd93476ae5e2827c2acb01f0615a4522cb8b2c9e
Ashley Claymore <acutmore@users.noreply.github.com>
2021-09-02 17:00:58 +0100

    reduce scope from 10 to 4 new methods

--- polyfill.d.ts
+++ polyfill.d.ts
@@ -1,132 +1,72 @@
 declare global {
     interface Array<T> {
-        withCopiedWithin(target: number, start?: number, end?: number): T[];
-        withFilled(v: T, start?: number, end?: number): T[];
-        withPopped(): T[];
-        withPushed(...values: T[]): T[];
+        withAt(index: number, value: T): T[];
         withReversed(): T[];
-        withShifted(): T[];
         withSorted(compareFn?: (a: T, b: T) => number): T[];
         withSpliced(start: number, deleteCount?: number, ...values: T[]): T[];
-        withUnshifted(...values: T[]): T[];
-        withAt(index: number, value: T): T[];
     }
 
     interface Int8Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Uint8Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Uint8ClampedArray {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Int16Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Uint16Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Int32Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Uint32Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Float32Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 
     interface Float64Array {
-        withCopiedWithin(target: number, start?: number, end?: number): this;
-        withFilled(v: number, start?: number, end?: number): this;
-        withPopped(): this;
-        withPushed(...values: number[]): this;
+        withAt(index: number, value: number): this;
         withReversed(): this;
-        withShifted(): this;
         withSorted(compareFn?: (a: number, b: number) => number): this;
         withSpliced(start: number, deleteCount?: number, ...values: number[]): this;
-        withUnshifted(...values: number[]): this;
-        withAt(index: number, value: number): this;
     }
 }
 export {};
--- polyfill.test.js
+++ polyfill.test.js
@@ -2,88 +2,16 @@
 require("./polyfill.js");
 const tape = require("tape");
 
-tape("Array.prototype.withCopiedWithin", (t) => {
-    const orig = [1, 2, 3, 4, 5];
-    const expected = [5, 2, 3, 4, 5];
-    const targetIndex = 0;
-    const sourceIndex = -1;
-
-    const copy = orig.withCopiedWithin(targetIndex, sourceIndex);
-
-    t.deepEqual(copy, expected);
-    t.notEqual(orig, copy);
-    t.notDeepEqual(orig, copy);
-    t.end();
-});
-
-tape("Array.prototype.withFilled", (t) => {
-    const orig = [1, 2, 3];
-    const expected = [4, 4, 4];
-    const fillValue = 4;
-
-    const copy = orig.withFilled(fillValue);
-
-    t.deepEqual(copy, expected);
-    t.notEqual(orig, copy);
-    t.notDeepEqual(orig, copy);
-    t.end();
-});
-
-tape("Array.prototype.withPopped", (t) => {
-    const orig = [1, 2, 3];
-    const expected = [1, 2];
-
-    const copy = orig.withPopped();
-
-    t.deepEqual(copy, expected);
-    t.notEqual(orig, copy);
-    t.notDeepEqual(orig, copy);
-    t.end();
-});
-
-tape("Array.prototype.withPopped works with array-like values", (t) => {
+tape("Array.prototype.withReversed works with array-like values", (t) => {
     const orig = {
-        0: 1,
+        0: 3,
         1: 2,
-        2: 3,
+        2: 1,
         length: 3
     };
-    const expected = [1, 2];
+    const expected = [1, 2, 3];
 
-    const copy = Array.prototype.withPopped.call(orig);
-
-    t.deepEqual(copy, expected);
-    t.notEqual(orig, copy);
-    t.notDeepEqual(orig, copy);
-    t.end();
-});
-
-tape("Array.prototype.withPopped reads minimum number of properties from callee", (t) => {
-    let readExcessProperties = false;
-    const orig = {
-        0: 1,
-        1: 2,
-        get "2"() {
-            readExcessProperties = true;
-            return 3;
-        },
-        length: 3
-    };
-    const expected = [1, 2];
-
-    const copy = Array.prototype.withPopped.call(orig);
-
-    t.deepEqual(copy, expected);
-    t.assert(!readExcessProperties);
-    t.end();
-});
-
-tape("Array.prototype.withPushed", (t) => {
-    const orig = [1, 2, 3];
-    const push = [4, 5, 6];
-    const expected = [1, 2, 3, 4, 5, 6];
-
-    const copy = orig.withPushed(...push);
+    const copy = Array.prototype.withReversed.call(orig);
 
     t.deepEqual(copy, expected);
     t.notEqual(orig, copy);
@@ -96,18 +24,6 @@ tape("Array.prototype.withReversed", (t) => {
     const expected = [1, 2, 3];
 
     const copy = orig.withReversed();
-
-    t.deepEqual(copy, expected);
-    t.notEqual(orig, copy);
-    t.notDeepEqual(orig, copy);
-    t.end();
-});
-
-tape("Array.prototype.withShifted", (t) => {
-    const orig = [1, 2, 3];
-    const expected = [2, 3];
-
-    const copy = orig.withShifted();
 
     t.deepEqual(copy, expected);
     t.notEqual(orig, copy);
@@ -150,19 +66,6 @@ tape("Array.prototype.withSpliced", (t) => {
     const ins = [2, 3];
 
     const copy = orig.withSpliced(idx, delNum, ...ins);
-
-    t.deepEqual(copy, expected);
-    t.notEqual(orig, copy);
-    t.notDeepEqual(orig, copy);
-    t.end();
-});
-
-tape("Array.prototype.withUnshifted", (t) => {
-    const orig = [4, 5, 6];
-    const unshift = [1, 2, 3];
-    const expected = [1, 2, 3, 4, 5, 6];
-
-    const copy = orig.withUnshifted(...unshift);
 
     t.deepEqual(copy, expected);
     t.notEqual(orig, copy);
@@ -221,43 +124,32 @@ tape(`Array does not use Symbol.species for the new methods`, (t) => {
     }
 
     assertType(orig.withAt(0, 0));
-    assertType(orig.withAt(0, 0));
-    assertType(orig.withCopiedWithin(0, 0, 0));
-    assertType(orig.withFilled(0));
-    assertType(orig.withPopped());
-    assertType(orig.withPushed(0));
     assertType(orig.withReversed());
-    assertType(orig.withShifted());
     assertType(orig.withSorted());
     assertType(orig.withSpliced(0, 0));
-    assertType(orig.withUnshifted(0));
 
     t.end();
 });
 
 tape("Array.prototype[Symbol.unscopables]", (t) => {
+    const methodNames = ['withAt', 'withReversed', 'withSorted', 'withSpliced'];
+
+    // ensure we are checking the correct methods names, otherwise test will always pass, regardless of Symbol.unscopables
+    for (const method of methodNames) {
+        t.equal(typeof Array.prototype[method], 'function');
+    }
+
     const marker = Symbol();
-    const copiedWithin = marker;
-    const filled = marker;
-    const popped = marker;
-    const pushed = marker;
-    const reversed = marker;
-    const shifted = marker;
-    const sorted = marker;
-    const spliced = marker;
-    const unshifted = marker;
+    const withAt = marker;
+    const withReversed = marker;
+    const withSorted = marker;
+    const withSpliced = marker;
 
     // @ts-expect-error: 'with' is unsupported
     with ([]) {
-        t.equal(copiedWithin, marker);
-        t.equal(filled, marker);
-        t.equal(popped, marker);
-        t.equal(pushed, marker);
-        t.equal(reversed, marker);
-        t.equal(shifted, marker);
-        t.equal(sorted, marker);
-        t.equal(spliced, marker);
-        t.equal(unshifted, marker);
+        for (const method of methodNames) {
+            t.equal(eval(method), marker);
+        }
     }
     t.end();
 });
@@ -273,75 +165,11 @@ tape("Array.prototype[Symbol.unscopables]", (t) => {
     Float32Array,
     Float64Array
 ].forEach((TypedArray) => {
-    tape(`${TypedArray.name}.prototype.withCopiedWithin`, (t) => {
-        const orig = new TypedArray([1, 2, 3, 4, 5]);
-        const expected = new TypedArray([5, 2, 3, 4, 5]);
-        const targetIndex = 0;
-        const sourceIndex = -1;
-
-        const copy = orig.withCopiedWithin(targetIndex, sourceIndex);
-
-        t.deepEqual(copy, expected);
-        t.notEqual(orig, copy);
-        t.notDeepEqual(orig, copy);
-        t.end();
-    });
-
-    tape(`${TypedArray.name}.prototype.withFilled`, (t) => {
-        const orig = new TypedArray([1, 2, 3]);
-        const expected = new TypedArray([4, 4, 4]);
-        const fillValue = 4;
-
-        const copy = orig.withFilled(fillValue);
-
-        t.deepEqual(copy, expected);
-        t.notEqual(orig, copy);
-        t.notDeepEqual(orig, copy);
-        t.end();
-    });
-
-    tape(`${TypedArray.name}.prototype.withPopped`, (t) => {
-        const orig = new TypedArray([1, 2, 3]);
-        const expected = new TypedArray([1, 2]);
-
-        const copy = orig.withPopped();
-
-        t.deepEqual(copy, expected);
-        t.notEqual(orig, copy);
-        t.notDeepEqual(orig, copy);
-        t.end();
-    });
-
-    tape(`${TypedArray.name}.prototype.withPushed`, (t) => {
-        const orig = new TypedArray([1, 2, 3]);
-        const push = [4, 5, 6];
-        const expected = new TypedArray([1, 2, 3, 4, 5, 6]);
-
-        const copy = orig.withPushed(...push);
-
-        t.deepEqual(copy, expected);
-        t.notEqual(orig, copy);
-        t.notDeepEqual(orig, copy);
-        t.end();
-    });
-
     tape(`${TypedArray.name}.prototype.withReversed`, (t) => {
         const orig = new TypedArray([3, 2, 1]);
         const expected = new TypedArray([1, 2, 3]);
 
         const copy = orig.withReversed();
-
-        t.deepEqual(copy, expected);
-        t.notEqual(orig, copy);
-        t.notDeepEqual(orig, copy);
-        t.end();
-    });
-
-    tape(`${TypedArray.name}.prototype.withShifted`, (t) => {
-        const orig = new TypedArray([1, 2, 3]);
-        const expected = new TypedArray([2, 3]);
-
-        const copy = orig.withShifted();
 
         t.deepEqual(copy, expected);
         t.notEqual(orig, copy);
@@ -384,19 +212,6 @@ tape("Array.prototype[Symbol.unscopables]", (t) => {
         const ins = [2, 3];
 
         const copy = orig.withSpliced(idx, delNum, ...ins);
-
-        t.deepEqual(copy, expected);
-        t.notEqual(orig, copy);
-        t.notDeepEqual(orig, copy);
-        t.end();
-    });
-
-    tape(`${TypedArray.name}.prototype.withUnshifted`, (t) => {
-        const orig = new TypedArray([4, 5, 6]);
-        const unshift = [1, 2, 3];
-        const expected = new TypedArray([1, 2, 3, 4, 5, 6]);
-
-        const copy = orig.withUnshifted(...unshift);
 
         t.deepEqual(copy, expected);
         t.notEqual(orig, copy);
@@ -457,15 +272,9 @@ tape("Array.prototype[Symbol.unscopables]", (t) => {
         const orig = new SubClass([1, 2, 3]);
 
         assertType(orig.withAt(0, 0));
-        assertType(orig.withCopiedWithin(0, 0, 0));
-        assertType(orig.withFilled(0));
-        assertType(orig.withPopped());
-        assertType(orig.withPushed(0));
         assertType(orig.withReversed());
-        assertType(orig.withShifted());
         assertType(orig.withSorted());
         assertType(orig.withSpliced(0, 0));
-        assertType(orig.withUnshifted(0));
 
         t.end();
     });
